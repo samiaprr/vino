c9d8b753ee4b0cf12a0ef3ad6f00ecb399af76a7
Jonathan Martel <jonathmartel@gmail.com>
2019-01-28 11:46:01 -0500

    Ajout du dossier Lib

---
+++ dataconf.php
@@ -0,0 +1,9 @@
+<?php
+// Remplir et renommer dataconf.php
+	define("BASEURL", "http://vino.jonathanmartel.info");
+
+	define('HOST', 'vinodb.db.6544725.79c.hostedresource.net');
+	define('USER', 'vinodb');
+	define('PASSWORD', 'Vino2019!');
+	define('DATABASE', 'vinodb');
+?>
